d0ec5a9e2995fc16be87320796e34fc9e5de63aa
Enyqzr <133854115+Enyqzr@users.noreply.github.com>
2023-10-04 14:56:11 +0200

    Trie gamme/prix

--- vueVip/src/views/VehiculeList.vue
+++ vueVip/src/views/VehiculeList.vue
@@ -1,34 +1,97 @@
 <script>
 import Vehicule from "@/components/Vehicule.vue";
+
 export default {
   name: "VehiculeList",
-  components:{
+  components: {
     Vehicule,
   },
-  methods:{
-    async fetchVehicule(){
-      const r= await fetch('http://192.168.1.202:8083/vehicules')
-      if (r.ok===true){
-         this.vehicules= await r.json()
+  computed: {
+    vehiculesFiltres() {
+      let vehiculeList = this.vehicules
+      if (this.appliedFilters?.gammeFiltre) {
+        console.log(vehiculeList)
+        vehiculeList = vehiculeList.filter(vehicule => vehicule.type === this.gammeFiltre)
       }
-      throw new Error('Impossible de contacter le serveur mat')
+      if (this.appliedFilters?.prixMin !== undefined && this.appliedFilters?.prixMax !== undefined) {
+        console.log(this.appliedFilters?.prixMin)
+        console.log(vehiculeList)
+        vehiculeList = vehiculeList.filter(vehicule => vehicule.prixReservation >= this.prixMin && vehicule.prixReservation <= this.prixMax);
+      }
+      console.log(vehiculeList)
+      return vehiculeList
+    }
+  },
+  data() {
+    return {
+      vehicules: [],
+      gammeFiltre: "",
+      prixMin: null, //prix min
+      prixMax: null, //prix max
+      appliedFilters: null,
+    }
+  },
+  methods: {
+    applyFilters() {
+      this.appliedFilters = {
+        gammeFiltre: this.gammeFiltre,
+        prixMin: this.prixMin,
+        prixMax: this.prixMax
+      }
+    },
+    async fetchVehicule() {
+      try {
+
+
+        const r = await fetch('http://192.168.1.202:8083/vehicules')
+        if (r.ok === true) {
+          this.vehicules = await r.json()
+        }
+      } catch (e) {
+        throw new Error('Impossible de contacter le serveur mat')
+      }
     }
   },
   created() {
     this.fetchVehicule()
   },
-  data(){
-    return{
-      vehicules:[]
-    }
-  }
+
 }
 
 </script>
 
 <template>
-<Vehicule v-for="vehicule in vehicules" :marque="vehicule.marque" :couleur="vehicule.couleur" :annee="vehicule.annee" :type="vehicule.type" :immatriculation="vehicule.immattriculationNb" :nb-cv-fiscaux="vehicule.cheveauxFiscNb"  :prix-reservation="vehicule.prixReservation" :nb-km="vehicule.nbKillometre" :prix-km="vehicule.prixKillometre" :src="vehicule.src" :model="vehicule.model">
-</Vehicule>
+
+  <div>
+    <div>
+      <label for="gamme">Gamme :</label>
+      <select id="gamme" v-model="gammeFiltre">
+        <option value="">Toutes les gammes</option>
+        <option value="utilitaire">Utilitaire</option>
+        <option value="moto">Moto</option>
+      </select>
+    </div>
+    <div>
+      <label for="prixMin">Prix minimum :</label>
+      <input type="number" id="prixMin" v-model="prixMin">
+      <label for="prixMin">Prix maximum :</label>
+      <input type="number" id="prixMax" v-model="prixMax">
+    </div>
+    <button @click="applyFilters">Appliquer les filtres</button>
+  </div>
+  <Vehicule
+      v-for="vehicule in vehiculesFiltres"
+      :key="vehicule.immatriculation"
+      :marque="vehicule.marque"
+      :couleur="vehicule.couleur"
+      :immatriculation="vehicule.immattriculationNb"
+      :nb-cv-fiscaux="vehicule.cheveauxFiscNb"
+      :prix-reservation="vehicule.prixReservation"
+      :nb-km="vehicule.nbKillometre"
+      :prix-km="vehicule.prixKillometre"
+      :src="vehicule.src"
+      :model="vehicule.model"
+  ></Vehicule>
 </template>
 
 <style scoped>
