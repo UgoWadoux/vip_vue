664562a05ee197b81d0f644b5a327d58636f679d
Enyqzr <133854115+Enyqzr@users.noreply.github.com>
2023-10-06 10:07:21 +0200

    Inscription -Math

--- vueVip/src/views/VehiculeList.vue
+++ vueVip/src/views/VehiculeList.vue
@@ -1,24 +1,22 @@
 <script>
 import Vehicule from "@/components/Vehicule.vue";
+import DriverInfo from "@/components/DriverInfo.vue";
 
 export default {
   name: "VehiculeList",
   components: {
+    DriverInfo,
     Vehicule,
   },
   computed: {
     vehiculesFiltres() {
       let vehiculeList = this.vehicules
       if (this.appliedFilters?.gammeFiltre) {
-        console.log(vehiculeList)
         vehiculeList = vehiculeList.filter(vehicule => vehicule.type === this.gammeFiltre)
       }
       if (this.appliedFilters?.prixMin !== undefined && this.appliedFilters?.prixMax !== undefined) {
-        console.log(this.appliedFilters?.prixMin)
-        console.log(vehiculeList)
         vehiculeList = vehiculeList.filter(vehicule => vehicule.prixReservation >= this.prixMin && vehicule.prixReservation <= this.prixMax);
       }
-      console.log(vehiculeList)
       return vehiculeList
     }
   },
@@ -29,6 +27,7 @@ export default {
       prixMin: null, //prix min
       prixMax: null, //prix max
       appliedFilters: null,
+      showDriverInfo: false,
     }
   },
   methods: {
@@ -38,6 +37,9 @@ export default {
         prixMin: this.prixMin,
         prixMax: this.prixMax
       }
+    },
+    toggleDriverInfo() {
+      this.showDriverInfo = !this.showDriverInfo
     },
     async fetchVehicule() {
       try {
@@ -61,39 +63,41 @@ export default {
 </script>
 
 <template>
-
   <div>
     <div>
-      <label for="gamme">Gamme :</label>
-      <select id="gamme" v-model="gammeFiltre">
-        <option value="">Toutes les gammes</option>
-        <option value="utilitaire">Utilitaire</option>
-        <option value="moto">Moto</option>
-      </select>
+      <div>
+        <label for="gamme">Gamme :</label>
+        <select id="gamme" v-model="gammeFiltre">
+          <option value="">Toutes les gammes</option>
+          <option value="utilitaire">Utilitaire</option>
+          <option value="moto">Moto</option>
+        </select>
+      </div>
+      <div>
+        <label for="prixMin">Prix minimum :</label>
+        <input type="number" id="prixMin" v-model="prixMin">
+        <label for="prixMin">Prix maximum :</label>
+        <input type="number" id="prixMax" v-model="prixMax">
+      </div>
+      <button @click="applyFilters">Appliquer les filtres</button>
     </div>
-    <div>
-      <label for="prixMin">Prix minimum :</label>
-      <input type="number" id="prixMin" v-model="prixMin">
-      <label for="prixMin">Prix maximum :</label>
-      <input type="number" id="prixMax" v-model="prixMax">
+    <div class="listVehicule">
+      <Vehicule
+          v-for="vehicule in vehiculesFiltres"
+          :key="vehicule.immatriculation"
+          :marque="vehicule.marque"
+          :couleur="vehicule.couleur"
+          :immatriculation="vehicule.immattriculationNb"
+          :nb-cv-fiscaux="vehicule.cheveauxFiscNb"
+          :prix-reservation="vehicule.prixReservation"
+          :nb-km="vehicule.nbKillometre"
+          :prix-km="vehicule.prixKillometre"
+          :src="vehicule.src"
+          :model="vehicule.model"
+      :id="vehicule.id"></Vehicule>
     </div>
-    <button @click="applyFilters">Appliquer les filtres</button>
-  </div>
-  <div class="listVehicule">
-    <Vehicule
-        v-for="vehicule in vehiculesFiltres"
-        :key="vehicule.immatriculation"
-        :marque="vehicule.marque"
-        :couleur="vehicule.couleur"
-        :immatriculation="vehicule.immattriculationNb"
-        :nb-cv-fiscaux="vehicule.cheveauxFiscNb"
-        :prix-reservation="vehicule.prixReservation"
-        :nb-km="vehicule.nbKillometre"
-        :prix-km="vehicule.prixKillometre"
-        :src="vehicule.src"
-        :model="vehicule.model"
-        :id="vehicule.id"
-    ></Vehicule>
+    <button @click="toggleDriverInfo">Saisir vos informations de conducteur</button>
+    <DriverInfo v-if="showDriverInfo"/>
   </div>
 </template>
 
